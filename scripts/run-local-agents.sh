063713226ef4b8f4827aeb640b2405172f661d94
James Wright <jsw@mahoganyteeth.com>
2026-07-13 16:00:40 +0100

    Updates local script to run under bash and to run a single Qwen 3 Coder Q5_K_S instance, also improving script name

--- scripts/run-local-agents.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# 5-bit quantised models that run across the two respective RTX 3090s
-llama-server -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q5_K_S -t 3 -dev CUDA0 -sm none -mg 0 &>/dev/null &
-llama-server -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q5_K_S -t 3 -dev CUDA1 -sm none -mg 0 --port 8081 &>/dev/null &
-
-# 3-bit quantised model that runs on the RTX 4060 Ti (16 GB) 
-llama-server -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q3_K_M -t 3 -dev CUDA2 -sm none -mg 0 --port 8082 &>/dev/null &
-
---
+++ scripts/run-local-models.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# 5-bit quantised models that run across the two respective RTX 3090s
+llama-server -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q5_K_S -t 4 -dev CUDA0,CUDA1 -sm none -mg 0 &>/dev/null &
+
+# 3-bit quantised model that runs on the RTX 4060 Ti (16 GB)
+llama-server -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q3_K_M -t 4 -dev CUDA1 -sm none -mg 0 --port 8081 &>/dev/null &
