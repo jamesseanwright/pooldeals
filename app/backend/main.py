2efbb42874b51f2a37efcdc968074c8accc1b7ca
Fylde Brain <hi+fylde-brain@james.engineering>
2026-07-23 19:21:54 +0100

    feat: bootstrap FastAPI app with main.py and pyproject.toml

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-app: FastAPI = FastAPI()
+app = FastAPI()
 
 
 @app.get("/")
-async def read_root() -> dict[str, str]:
-    return {"message": "Hello, World!"}
+def read_root():
+    return {"Hello": "World"}
